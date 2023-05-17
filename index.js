d00f1080887890c718b0648eeb8aaec05028ac1c
ajkn1904 <ajknishat@gmail.com>
2023-05-17 13:38:21 +0600

    api for getting data is added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
@@ -13,15 +13,74 @@ app.use(cors());
 
 
 
-mongoose.connect(process.env.DATABASE).then(()=> {
+/*
+mongoose.connect(process.env.DATABASE).then(() => {
     console.log("Database Connected Successfully.");
 })
 
 
-
-
-app.get('/', async (req, res) => {
-    res.send('api-mongo server is running')
+const studentSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    sec: String,
+    roll: Number,
+    grade: String
 });
 
-app.listen(port, () => console.log(`api-mongo server is running on ${port}`));
+
+const Student = mongoose.model('Student', studentSchema)
+
+ app.post('/api/v1/student', (req, res,next)=>{
+    res.send('working');
+}) 
+
+ app.get('/api/v1/students', async (req, res) => {
+    try{
+        const students = await Student.find({});
+        res.send(students)
+        }
+        catch(e){
+            res.status(400).json({
+                error: e.massage,
+            })
+            }           
+            
+       }) */
+
+
+const uri = process.env.DATABASE;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+
+        await client.connect();
+
+        const studentsCollection = client.db('apiMongoose').collection('students');
+
+
+        app.get('/students', async (req, res) => {
+            const query = { };
+            const result = await studentsCollection.find(query).toArray()
+            res.send(result)
+        })
+
+
+
+         }
+        finally {
+    
+        }
+    }
+    run().catch(console.log);
+    
+    
+
+
+        app.get('/', async (req, res) => {
+            res.send('api-mongo server is running')
+        });
+
+        app.listen(port, () => console.log(`api-mongo server is running on ${port}`));
